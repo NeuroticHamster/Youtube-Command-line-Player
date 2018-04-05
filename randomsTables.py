dce1e1653592e3d3694e6b623c88d39ecff9f64a
NeuroticHamster <35155973+NeuroticHamster@users.noreply.github.com>
2018-04-05 00:35:00 -0400

    Update randomsTables.py
    removed example code

--- randomsTables.py
+++ randomsTables.py
@@ -7,26 +7,7 @@ from sqlalchemy import create_engine
 
 
 Base = declarative_base()
-'''
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    songname = Column(String(50))
 
-#-------
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)'''
 
 class Songinfo(Base):
     __tablename__ = 'songinfo'
@@ -35,11 +16,7 @@ class Songinfo(Base):
     artistname = Column(String(50))
     songname = Column(String(50))
     link = Column(String(100))
-# Create an engine that stores data in the local directory's
-# sqlalchemy_example.db file.
-#engine = create_engine('sqlite:///sqlalchemy_example.db')
+
 engine = create_engine('sqlite:///music.db')
 
-# Create all tables in the engine. This is equivalent to "Create Table"
-# statements in raw SQL.
 Base.metadata.create_all(engine)
